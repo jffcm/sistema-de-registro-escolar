f8deded08ba7110e6275bfdaffdf95ff083a0777
João Martins <joao.jffc@gmail.com>
2023-10-06 19:51:46 -0300

    Initial commit

--- regescweb/src/main/java/me/joaof/regescweb/controllers/ProfessorController.java
+++ regescweb/src/main/java/me/joaof/regescweb/controllers/ProfessorController.java
@@ -1,28 +1,31 @@
 package me.joaof.regescweb.controllers;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.validation.Valid;
-import me.joaof.regescweb.dto.RequisicaoNovoProfessor;
+import me.joaof.regescweb.dto.ProfessorDto;
 import me.joaof.regescweb.models.Professor;
 import me.joaof.regescweb.models.StatusProfessor;
 import me.joaof.regescweb.repositories.ProfessorRepository;
 
+
 @Controller
+@RequestMapping("/professores")
 public class ProfessorController {
 	@Autowired
 	private ProfessorRepository professorRepository;
 	
 	
-	@GetMapping("/professores")
+	@GetMapping
 	public ModelAndView index() {
 		List<Professor> professores = professorRepository.findAll();
 		
@@ -31,26 +34,88 @@ public class ProfessorController {
 		return mv;
 	}
 	
-	@GetMapping("/professores/new")
-	public ModelAndView novoProfessor() {
+	@GetMapping("/new")
+	public ModelAndView novoProfessor(ProfessorDto professorDto) {
 		ModelAndView mv = new ModelAndView("professores/new");
 		mv.addObject("statusProfessor", StatusProfessor.values());
 		return mv;
 	}
 	
-	@PostMapping("/professores")
-	public String create(@Valid RequisicaoNovoProfessor requisicaoNovoProfessor, BindingResult bindingResult) {
+	@PostMapping
+	public ModelAndView create(@Valid ProfessorDto professorDto, BindingResult bindingResult) {
 		if (bindingResult.hasErrors()) {
-			return "redirect:professores/new";
+			System.err.println("\nERRORS FOUND: " + bindingResult.getErrorCount());
+			
+			ModelAndView mv = new ModelAndView("professores/new");
+			mv.addObject("statusProfessor", StatusProfessor.values());
+			return mv;
 		}
-		professorRepository.save(requisicaoNovoProfessor.toProfessor());
-		return "redirect:/professores";
+		
+		professorRepository.save(professorDto.toProfessor());
+		return new ModelAndView("redirect:/professores");
+	}
+		
+	@GetMapping("/{id}")
+	public ModelAndView show(@PathVariable long id) {
+		Optional<Professor> optional = professorRepository.findById(id);
+		
+		if (optional.isPresent()) {
+			Professor professor = optional.get();
+			ModelAndView mv = new ModelAndView("professores/show");
+			mv.addObject("professor", professor);
+			return mv;
+		} else {
+			return new ModelAndView("redirect:/professores");
+		}
+	
 	}
 	
-	@DeleteMapping("/professores/{id}")
-	public String destroy(@PathVariable long id) {
-		professorRepository.deleteById(id);
-		return "redirect:/professores";
+	@GetMapping("/{id}/edit")
+	public ModelAndView edit(@PathVariable long id, ProfessorDto professorDto) {
+		Optional<Professor> optional = professorRepository.findById(id);
+		
+		if (optional.isPresent()) {
+			Professor professor = optional.get();
+			professorDto.fromProfessor(professor);
+			
+			ModelAndView mv = new ModelAndView("professores/edit");
+			mv.addObject("statusProfessor", StatusProfessor.values());
+			mv.addObject("id", professor.getId());
+			return mv;
+		} else {
+			return new ModelAndView("redirect:/professores");
+		}
 	}
 	
+	@PostMapping("/{id}")
+	public ModelAndView update(@PathVariable long id, @Valid ProfessorDto professorDto, BindingResult bindingResult){
+		if (bindingResult.hasErrors()) {
+			ModelAndView mv = new ModelAndView("professores/edit");
+			mv.addObject("statusProfessor", StatusProfessor.values());
+			return mv;
+		} else {
+			Optional<Professor> optional = professorRepository.findById(id);
+			
+			if (optional.isPresent()) {
+				Professor professor = optional.get();
+				professorRepository.save(professorDto.toProfessor(professor));
+				
+				return new ModelAndView("redirect:/professores/" + professor.getId());
+				
+			} else {
+				return new ModelAndView("redirect:/professores");
+			}
+		}
+	}
+	
+	@GetMapping("/{id}/delete")
+	public String delete(@PathVariable long id) {
+		if (professorRepository.existsById(id)) {
+			professorRepository.deleteById(id);
+			return "redirect:/professores";
+		} else {
+			return "redirect:/professores";
+		}
+	}
+
 }
--- regescweb/src/main/java/me/joaof/regescweb/dto/ProfessorDto.java
+++ regescweb/src/main/java/me/joaof/regescweb/dto/ProfessorDto.java
@@ -1,55 +1,51 @@
 package me.joaof.regescweb.dto;
-
 import java.math.BigDecimal;
 
-//import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import me.joaof.regescweb.models.Professor;
 import me.joaof.regescweb.models.StatusProfessor;
 
 // DTO
-public class RequisicaoNovoProfessor {
-	@NotBlank
-	@NotNull
+public class ProfessorDto {
+	@NotBlank(message="O nome do professor não deve estar em branco.")
 	private String nome;
-	//@DecimalMin(value = "0")
+	
+	@NotNull(message="O campo salário não deve estar nulo.")
+	@DecimalMin(value="0.0", message="O campo salário deve ser um número maior ou igual a 0.0.")
+	@DecimalMax(value="1000000", message="O campo salário deve ser um número menor ou igual a 1.000.000.")
 	private BigDecimal salario;
+	
 	private StatusProfessor statusProfessor;
 	
-	public RequisicaoNovoProfessor() {}
+	public ProfessorDto() {}
 
-	
 	public String getNome() {
 		return nome;
 	}
-
 
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
-
 	public BigDecimal getSalario() {
 		return salario;
 	}
-
 
 	public void setSalario(BigDecimal salario) {
 		this.salario = salario;
 	}
 
-
 	public StatusProfessor getStatusProfessor() {
 		return statusProfessor;
 	}
-
 
 	public void setStatusProfessor(StatusProfessor statusProfessor) {
 		this.statusProfessor = statusProfessor;
 	}
 
-	
 	public Professor toProfessor() {
 		Professor professor = new Professor();
 		professor.setNome(this.nome);
@@ -58,12 +54,25 @@ public class RequisicaoNovoProfessor {
 		
 		return professor;
 	}
-
 	
+	public Professor toProfessor(Professor professor) {
+		professor.setNome(this.nome);
+		professor.setSalario(this.salario);
+		professor.setStatusProfessor(this.statusProfessor);
+		
+		return professor;
+	}
+	
+	public void fromProfessor(Professor professor) {
+		this.nome = professor.getNome();
+		this.salario = professor.getSalario();
+		this.statusProfessor = professor.getStatusProfessor();
+		
+	}
+
 	@Override
 	public String toString() {
-		return "RequisicaoNovoProfessor [nome=" + nome + ", salario=" + salario + ", statusProfessor=" + statusProfessor
-				+ "]";
+		return "ProfessorDto [nome=" + nome + ", salario=" + salario + ", statusProfessor=" + statusProfessor + "]";
 	}
 	
 }
